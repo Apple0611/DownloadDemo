d6eb0dbd39704ab002524fe2e5cf319dc4047412
downjoymac <downjoymac@thilongtekiMac-mini.local>
2014-07-30 18:15:08 +0800

    Initial Commit

---
+++ DownloadDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DownloadDemo
+//
+//  Created by downjoymac on 14-7-30.
+//  Copyright (c) 2014年 Apple0611. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DownloadDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DownloadDemo
+//
+//  Created by downjoymac on 14-7-30.
+//  Copyright (c) 2014年 Apple0611. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
